19839206467f869feef1d24fbddc7bddf37bf54a
Aventuras Chinese Build <aventuras@chinese.local>
2026-07-23 15:09:53 +0800

    Fix duplicate t() import in ActionInput

--- src/lib/components/story/ActionInput.svelte
+++ src/lib/components/story/ActionInput.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { t } from '$lib/i18n';
   import { tick } from 'svelte'
   import { ui } from '$lib/stores/ui.svelte'
   import { story } from '$lib/stores/story.svelte'
